cdc1d7e92d32cab8c6154c43191641301a67bb07
ImbaMaksym <137620059+ImbaMaksym@users.noreply.github.com>
2023-11-19 23:57:59 +0200

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+handlerGallery();
+
+function handlerGallery() {
+  createGallery();
+
+  let lightbox = new SimpleLightbox(".gallery__link", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+function createGallery() {
+  const galleryItemsHTML = galleryItems.map(createGalleryItem).join("");
+  galleryList.innerHTML = galleryItemsHTML;
+}
+
+function createGalleryItem({ preview, original, description }) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
+}
